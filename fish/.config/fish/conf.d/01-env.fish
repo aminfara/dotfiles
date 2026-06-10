9e8e628e4a32a9f661eee272cd92f71a4c818bde
Ali 🚶 <ali.aminfar@gmail.com>
2026-06-10 10:16:34 +1000

    [fish] fix unwanted login message

--- fish/.config/fish/conf.d/01-env.fish
+++ fish/.config/fish/conf.d/01-env.fish
@@ -1,11 +1,11 @@
 # XDG paths, locale, editor.
 
 set -q XDG_CONFIG_HOME; or set -gx XDG_CONFIG_HOME $HOME/.config
-set -q XDG_DATA_HOME;   or set -gx XDG_DATA_HOME   $HOME/.local/share
-set -q XDG_CACHE_HOME;  or set -gx XDG_CACHE_HOME  $HOME/.cache
-set -q XDG_STATE_HOME;  or set -gx XDG_STATE_HOME  $HOME/.local/state
+set -q XDG_DATA_HOME; or set -gx XDG_DATA_HOME $HOME/.local/share
+set -q XDG_CACHE_HOME; or set -gx XDG_CACHE_HOME $HOME/.cache
+set -q XDG_STATE_HOME; or set -gx XDG_STATE_HOME $HOME/.local/state
 
-set -gx LANG   en_US.UTF-8
+set -gx LANG en_US.UTF-8
 set -gx LC_ALL en_US.UTF-8
 
 set -gx TERM xterm-256color
@@ -17,12 +17,12 @@ if command -q nvim
 end
 
 if command -q bat
-    set -gx MANROFFOPT -c  # prevent raw escape codes in man pages piped through bat
-    set -gx BAT_PAGER  less
-    set -gx MANPAGER   "sh -c 'col -bx | bat --style=plain --language=man'"
-    set -gx PAGER      "bat --paging=always --style=plain"
+    set -gx MANROFFOPT -c # prevent raw escape codes in man pages piped through bat
+    set -gx BAT_PAGER less
+    set -gx MANPAGER "sh -c 'col -bx | bat --style=plain --language=man'"
+    set -gx PAGER "bat --paging=always --style=plain"
 end
 
 if command -q ssh-agent
-   eval $(ssh-agent -c)
+    eval $(ssh-agent -c) >/dev/null
 end
