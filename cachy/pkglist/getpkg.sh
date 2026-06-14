da880c7c0c5cb602dfb89bf19dd6b59db6704f04
Ali 🚶 <ali.aminfar@gmail.com>
2026-06-14 11:58:54 +1000

    [cachy] update packages list - original KDE desktop

--- cachy/pkglist/getpkg.sh
+++ cachy/pkglist/getpkg.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 EXP_FILE=explicitly_installed_packages.txt
+AUR_FILE=explicitly_installed_aur_packages.txt
 OPT_FILE=optional_packages_installed_asdeps.txt
 
-
 echo "Exporting list of explicitly installed packages to '$EXP_FILE'..."
-pacman -Qqe > "$EXP_FILE"
+pacman -Qqen > "$EXP_FILE"
+
+echo "Exporting list of explicitly installed AUR packages to '$AUR_FILE'..."
+pacman -Qqm > "$AUR_FILE"
 
 echo "Exporting list of optional packages installed (asdeps) to '$OPT_FILE' ..."
 comm -13 <(pacman -Qqdt | sort) <(pacman -Qqdtt | sort) > "$OPT_FILE"
